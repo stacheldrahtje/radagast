1fe1f3c2cbd372aa6039f86fe8b12158ca4f0bd4
Natasha Ament-Teusink <stacheldrahtje@gmail.com>
2023-09-25 12:54:54 +0200

    added onclicked()

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -13,3 +13,9 @@ MainWindow::~MainWindow()
     delete ui;
 }
 
+
+void MainWindow::on_nextButton_clicked()
+{
+    ui->stackedWidget->setCurrentIndex(1);
+}
+
--- mainwindow.h
+++ mainwindow.h
@@ -15,6 +15,9 @@ public:
     MainWindow(QWidget *parent = nullptr);
     ~MainWindow();
 
+private slots:
+    void on_nextButton_clicked();
+
 private:
     Ui::MainWindow *ui;
 };
