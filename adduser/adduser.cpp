59eaf76128ea21cb57f81d737235fc7b97c6edcc
Natasha Ament-Teusink <nwj.ament@gmail.com>
2023-11-30 09:33:51 +0100

    insert adduser example code.

--- adduser/adduser.cpp
+++ adduser/adduser.cpp
@@ -2,5 +2,14 @@
 #include <iostream>
 
 int createuser (QString uname, QString fname);
+{
+    char username[16]="<username>";
+    char fullname[16]="<fullname>";
+    string combi = username;
+    combi += " -c ";
+    combi += fullname;
+    cout << combi << endl;
+    return 0;
+}
 char* hashpasswd (QString);
 char* fnc_stdout (QString);
