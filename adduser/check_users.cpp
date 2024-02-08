9fcc35099011f5ff7cddb56bbf493869f313bda1
Natasha Ament-Teusink <nwj.ament@gmail.com>
2024-02-08 07:35:26 +0100

    Update check_users.cpp

--- adduser/check_users.cpp
+++ adduser/check_users.cpp
@@ -1,4 +1,15 @@
 #include <iostream>
 #include <string>
 #include <fstream>
+
+bool std::check_users (string uname)
+{
+  std::fstream file_object;
+  std::file_object.open ("/etc/passwd", ios::in);
+  std::string line_read;
+  while (file_object, line_read)
+  {
+    
+  }
+
 //check_users file
